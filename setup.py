33c3ba4c3d6f9110c9cb1997573853255e2df530
Gopinath Langote <gopinathlangote11@gmail.com>
2018-07-17 16:42:11 +0530

    Modify setup.py

--- setup.py
+++ setup.py
@@ -27,17 +27,6 @@ setup(
     entry_points={'console_scripts': ['keepcli = Main.__main__:main']},
     author='gopinathlangote',
     packages=['KeePass'],
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Utilities",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-    ],
     include_package_data=True,
     author_email='',
     description=''
