884eb8befddc1395afbd97c074818375a2625cb8
Gopinath Langote <gopinathlangote11@gmail.com>
2018-07-17 16:11:25 +0530

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 from distutils.core import setup
 
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def requirements():
+    with open('requirements.txt') as f:
+        install_requires = [line.strip() for line in f]
+
+    # Terminal colors for Windows
+    if 'win32' in sys.platform.lower():
+        install_requires.append('colorama')
+
+    return install_requires
+
+
 setup(
     name='keepass',
     version='1.0',
@@ -77,7 +96,20 @@ setup(
               'venv.lib.python2.7.site-packages.pkg_resources._vendor.packaging'],
     url='',
     license='',
+    install_requires=requirements(),
     author='gopinathlangote',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+    include_package_data=True,
     author_email='',
     description=''
 )
